f4fcab9f8da7fbac1355760d26e5c2e3954e406c
philpinto <philipdennispinto@icloud.com>
2019-12-02 13:12:30 -0800

    Updates

---
+++ precache-manifest.cc94bb3b0dddbc1ea24f67bd49cb918d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e7dc68d1641ee2b749dece6cac17645",
+    "url": "/directoryuser/index.html"
+  },
+  {
+    "revision": "2bb39c597460a196537c",
+    "url": "/directoryuser/static/css/main.5d1a1003.chunk.css"
+  },
+  {
+    "revision": "82b7557759b51cb27cf8",
+    "url": "/directoryuser/static/js/2.bfbb5337.chunk.js"
+  },
+  {
+    "revision": "2bb39c597460a196537c",
+    "url": "/directoryuser/static/js/main.ac50e13e.chunk.js"
+  },
+  {
+    "revision": "f499da7ce4e8ae7de6a1",
+    "url": "/directoryuser/static/js/runtime~main.9056cf47.js"
+  }
+]);
